8b48ee937c0859a1b1b3a13c10e3e6101f761b20
kejihela <72225471+kejihela@users.noreply.github.com>
2024-08-04 23:08:58 +0100

    Batch prefect deployment

--- Batch-deployment-s3/FlightPriceForcast_original.py
+++ Batch-deployment-s3/FlightPriceForcast_original.py
@@ -13,18 +13,23 @@ import pickle
 from sklearn.pipeline import make_pipeline
 import mlflow
 import uuid
+from prefect_aws import S3Bucket
+from prefect import task, flow, get_run_logger
 
 
 # %%
 output_file = 'prediction.csv'
-input_file = "dataset/flight_dataset.csv"
+#input_file = "https://raw.githubusercontent.com/kejihela/DataTalksclubProject/master/dataset/flight_dataset.csv"
 
 RUN_ID="16aa4ec2992e4def9a579a09802f7d54"
 
 
 # %%
-def load_data(input_file):
-    df= pd.read_csv(input_file)
+def load_data():
+    s3_bucket_data = S3Bucket.load("my-s3-bucket")
+    s3_bucket_data.download_folder_to_path(from_folder ="dataset", to_folder="dataset")
+    df= pd.read_csv("dataset/flight_dataset.csv")
+    #df= pd.read_csv(input_file)
     categorical = ["Airline", "Source", "Destination"]
     numerical = ["Total_Stops","Duration_hours","Duration_min"]
     df = df[categorical + numerical]
@@ -57,11 +62,15 @@ def load_model(RUN_ID):
     return model
 
 # %%
-def apply_model(RUN_ID, output_file, input_file):
-    data = load_data(input_file)
+@task 
+def apply_model(RUN_ID, output_file):
+    print("Loading data from GitHub...")
+    data = load_data()
+    print("Preprocessing dataset...")
     dict_df = preprocess_data_to_dict(data)
+    print("Loading model...")
     model = load_model(RUN_ID)
-
+    print("Performing batch prediction...")
     pred = model.predict(dict_df)
     df_result  = pd.DataFrame()
 
@@ -74,16 +83,22 @@ def apply_model(RUN_ID, output_file, input_file):
     df_result['predicted_duration'] = pred
     df_result['Loss'] = df_result['predicted_duration'] - df_result['duration']
     df_result['model_version'] = RUN_ID
-
+    print("Saving file...")
     df_result.to_csv(output_file)
+
+@flow
+def batch_prediction():
+    apply_model(
+        RUN_ID, 
+        output_file)
+
 
 
 # %%
 def run():
     apply_model(
         RUN_ID, 
-        output_file, 
-        input_file)
+        output_file)
 
 
 
