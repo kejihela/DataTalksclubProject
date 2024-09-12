b72110796db2406ad8780d88cc56fcb7ed423f67
Ubuntu <ubuntu@ip-172-31-46-206.eu-north-1.compute.internal>
2024-09-12 09:23:07 +0000

    monitoring

--- Deployment-webservice-s3/Streaming/lambda_function.py
+++ Deployment-webservice-s3/Streaming/lambda_function.py
@@ -2,29 +2,54 @@ import json
 import os
 import base64
 import boto3
-def predict(feature):
-    return 10.0
+import mlflow
+
     
 boto3_kinesis = boto3.client("kinesis") 
 PREDICTIONS_STREAM_NAME = os.getenv('PREDICTIONS_STREAM_NAME', 'kinesis_mlzoomcamp')
 
+TEST_RUN = os.getenv('TEST_RUN', 'False') == 'True'
+RUN_ID="16aa4ec2992e4def9a579a09802f7d54"
+
+logged_model = f's3://mlop-zoomcamp-adebayo/3/{RUN_ID}/artifacts/model'
+    # Load model as a PyFuncModel.
+model = mlflow.pyfunc.load_model(logged_model)
+    
+
+
+def preprocess_data_to_dict(df):
+    '''{
+    "ride": {
+        "Airline": 'IndiGo',
+        "Source": 'Banglore',
+        "Destination": New Delhi,
+        "Total_Stops": 0
+    }, 
+    "ride_id": 123
+}'''
+    df = df.to_dict(orient = "records")
+    return df
+
+def predict(features):
+    pred = model.predict(features)
+    return float(pred[0])
 
 def lambda_handler(event, context):
     predictions_events = []  
-    
-    for records in event['Records']:
-        data_encoded = records['kinesis']['data']
-        decoded_data = base64.b64decode(data_encoded).decode('utf-8')
-        print(decoded_data)
-        ride_events = json.loads(decoded_data)
-        
+    if not TEST_RUN:
+        for records in event['Records']:
+            data_encoded = records['kinesis']['data']
+            decoded_data = base64.b64decode(data_encoded).decode('utf-8')
+            print(decoded_data)
+            ride_events = json.loads(decoded_data)
+            
     
         
     ride = ride_events['ride']
     ride_id = ride_events['ride_id']
     
     
-    
+    print(ride)
     prediction = predict(ride)
     
 
